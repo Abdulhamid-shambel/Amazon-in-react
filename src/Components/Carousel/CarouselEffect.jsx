4772e32f710c1bdb24e7aa4884f0c6a270c23a04
Abdulhamid-shambel <abdulhamidshambel111@gmail.com>
2025-04-23 21:46:17 -0700

    Carousel Effect

---
+++ src/Components/Carousel/CarouselEffect.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import classes from './carousel.module.css'
+import {img} from './img/data'
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+
+function CarouselEffect() {
+  return (
+    <div>
+        <Carousel className={classes.carousel}
+            autoPlay={true}
+            infiniteLoop={true}
+            showIndicators={false}
+            showThumbs={false}
+        >
+            {
+                img.map((imageItemLink) => {
+                    return <img src={imageItemLink} alt="" />
+                })
+            }
+        </Carousel>
+
+        <div className={classes.hero_img}></div>
+    </div>
+  )
+}
+
+export default CarouselEffect
