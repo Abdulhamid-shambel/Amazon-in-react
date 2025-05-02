d1a9ab9c504139c017d23515a70c29bf841be3ba
Abdulhamid-shambel <abdulhamidshambel111@gmail.com>
2025-05-01 21:46:03 -0700

    Header Routing

---
+++ src/Pages/ProductDetail/ProductDetail.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import classes from "./productdetail.module.css";
+import LayOut from '../../Components/LayOut/LayOut';
+
+function ProductDetail() {
+
+  return (
+    <LayOut>
+      <div>Product detail</div>
+    </LayOut>
+  );
+}
+
+export default ProductDetail
