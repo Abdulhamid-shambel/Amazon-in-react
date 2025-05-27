d947f4af2d1b9dd57be68fcedfca813d6abfc3ab
Abdulhamid-shambel <abdulhamidshambel111@gmail.com>
2025-05-26 20:42:43 -0700

    Client side Payment Functionality

--- functions/index.js
+++ functions/index.js
@@ -23,7 +23,7 @@ app.get("/", (req, res) => {
 exports.api = onRequest(app);
 
 app.post("/payment/create", async (req, res) => {
-    const total = req.query.total;
+    const total = parseInt(req.query.total);
     if (total > 0) {
         const paymentIntent = await stripe.paymentIntents.create({
             amount: total,
--- src/Pages/Payment/Payment.jsx
+++ src/Pages/Payment/Payment.jsx
@@ -5,6 +5,10 @@ import { DataContext } from "../../Components/DataProvider/DataProvider";
 import ProductCard from "../../Components/Product/ProductCard";
 import { useStripe, useElements, CardElement } from "@stripe/react-stripe-js";
 import CurrencyFormat from "../../Components/CurrencyFormat/CurrencyFormat";
+import { axiosInstance } from "../../Api/axios";
+import ClipLoader from "react-spinners/ClipLoader";
+import { db } from "../../utility/firebase";
+import { useNavigate } from "react-router-dom";
 
 function Payment() {
   const [{ user, basket }] = useContext(DataContext);
@@ -18,12 +22,67 @@ function Payment() {
   }, 0);
 
   const [cardError, setCardError] = useState(null);
+  const [isProcessing, setIsProcessing] = useState(false);
 
   const stripe = useStripe();
   const elements = useElements();
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
+
+    // console.log(e);
+
     e?.error?.message? setCardError(e.error.message) : setCardError(null);
+  }
+
+  const handlePayment = async (e) => {
+    e.preventDefault();
+    try {
+      setIsProcessing(true);
+
+      // 1, backend || functions -----> contact to the client secret
+      
+      const response = await axiosInstance({
+        method: "post",
+        url: `/payment/create?total=${total * 100}`
+      });
+
+      // console.log(response.data);
+
+      const clientSecret = response.data.clientSecret;
+
+      // 2, client side (react side confirmation)
+
+      const {paymentIntent} = await stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: elements.getElement(CardElement)
+        }
+      });
+
+      // console.log(paymentIntent);
+
+
+      // 3, after the confirmation ---> order firestore database save & clear basket
+
+      await db.collection("users")
+        .doc(user?.uid)
+        .collection("orders")
+        .doc(paymentIntent.id)
+        .set({
+          basket: basket,
+          amount: paymentIntent.amount,
+          created: paymentIntent.created
+        });
+
+      setIsProcessing(false);
+
+      navigate("/orders", {state: {message: "Your order has been placed successfully"}});
+
+
+    } catch (error) {
+      console.log(error)
+      setIsProcessing(false);
+    }
   }
   return (
     <LayOut>
@@ -60,7 +119,7 @@ function Payment() {
           <h3>Payment Method</h3>
           <div className={classes.payment_card_container}>
             <div>
-              <form action="">
+              <form onSubmit={handlePayment}>
                 {/* card element */}
                 <CardElement onChange={handleChange} />
 
@@ -76,7 +135,23 @@ function Payment() {
                       <p>Total order |</p> <CurrencyFormat amount={total} />
                     </span>
                   </div>
-                  <button>Pay Now</button>
+                  <button type="submit">
+                    {isProcessing ? (
+                      <div className={classes.loading}>
+
+                        <ClipLoader
+                          color="gray"
+                          size={12}
+                          cssOverride={{ marginLeft: "10px" }}
+                        />
+                        <p>Please wait ....</p>
+                      </div>
+                      
+                    ) : (
+                      "Pay Now"
+                    )
+                    }
+                  </button>
                 </div>
               </form>
             </div>
